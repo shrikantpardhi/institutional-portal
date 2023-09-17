3bcde0e6d71a8bfcb40db22de7f9d0494db3cee0
shrikantpardhi <shripardhi92@gmail.com>
2023-09-17 20:08:24 +0530

    initial commit

---
+++ src/main/java/org/dynast/institutional/InstitutionalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.dynast.institutional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutionalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dynast/institutional/InstitutionalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dynast.institutional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
